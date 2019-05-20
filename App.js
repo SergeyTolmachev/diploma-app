2b7df9c36df6f654d5b402c444e0df8dd2a8dacd
SergeyTolmachev <stolmachov@gmail.com>
2019-05-20 22:57:28 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+
+
+export default class App extends React.Component {
+
+  handlePressButton = () => {
+    console.log();
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          title="Home"
+          onPress={this.handlePressButton}
+        />
+        <Button
+          title="Sign In"
+          onPress={this.handlePressButton}
+        />
+        <Button
+          title="Sign Up"
+          onPress={this.handlePressButton}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
